2033a16623ff3ba2f83d0115a72e9afc324a84ac
pabloeig <pmt24gr033@student.esgerencia.com>
2025-12-02 10:37:45 +0100

    fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Segundapull } from './segundapull/segundapull';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Segundapull],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
